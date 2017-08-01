f88fdb313c3a3a5ff4edb23d8c46d9525ff1f67c
shaudai <shaudai0402@hotmail.com>
2017-08-01 21:09:55 +0800

    Remove 'undefined index' message while the page was loaded in the beginning

--- index.php
+++ index.php
@@ -2,29 +2,32 @@
 
     $weather = "";
     $error = "";
+    
+    if ( isset( $_GET['city'] ) ) {
 
-    if ($_GET['city']) {
-        
-        $city = str_replace(" ", "", $_GET['city']);
-        
-        $file_headers = @get_headers("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
-        
-        if ($file_headers[0] == 'HTTP/1.1 404 Not Found') {
+        if ($_GET['city']) {
             
-            $error = "That city could not be found.";
+            $city = str_replace(" ", "", $_GET['city']);
             
-        } else {
+            $file_headers = @get_headers("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
             
-            $forcastPage = file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
+            if ($file_headers[0] == 'HTTP/1.1 404 Not Found') {
+                
+                $error = "That city could not be found.";
+                
+            } else {
+                
+                $forcastPage = file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
+                
+                $pageArray = explode('3 Day Weather Forecast Summary:</b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">', $forcastPage);
+                
+                $secondPageArray = explode('</span></span></span>', $pageArray[1]);
+                
+                $weather = $secondPageArray[0];
             
-            $pageArray = explode('3 Day Weather Forecast Summary:</b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">', $forcastPage);
+            }
             
-            $secondPageArray = explode('</span></span></span>', $pageArray[1]);
-            
-            $weather = $secondPageArray[0];
-        
         }
-        
     }
 
 ?>
@@ -88,7 +91,7 @@
         <form>
             <div class="form-group">
                 <label for="city">Enter the name of a city.</label>
-                <input type="text" class="form-control" name="city" id="city" placeholder="Eg. London, Tokyo" value = "<?php echo $_GET['city']; ?>">
+                <input type="text" class="form-control" name="city" id="city" placeholder="Eg. London, Tokyo" value = "<?php if( isset( $_GET['city'] ) ) echo $_GET['city']; ?>">
             </div>
 
             <button type="submit" class="btn btn-primary">Submit</button>
